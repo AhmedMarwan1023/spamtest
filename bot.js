1c7c738e193ab1188ca191243e5784e0ea18e91a
AhmedMarwan1023 <44772647+AhmedMarwan1023@users.noreply.github.com>
2018-12-23 13:18:42 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,9 +4,9 @@ console.log("Scrpit By Dream");
 
 
 client.on("ready", () => {
-let channel =     client.channels.get("هنا اي دي الروم")
+let channel =     client.channels.get("526372495985737728")
 setInterval(function() {
-channel.send(`هنا كلام ال سبام`);
+channel.send(`**طز سبام بس هيهيهيهي**`);
 }, 30)
 })
 
